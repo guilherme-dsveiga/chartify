69464c74f91fe1385dd16df419f3c002a56271ce
Guilherme <guilherme.dsveiga@gmail.com>
2022-06-25 04:00:42 -0300

    implementing login with spotify card

--- src/components/SpotifyLogin.jsx
+++ src/components/SpotifyLogin.jsx
@@ -1,7 +1,20 @@
+import { SpotifyLogo } from "phosphor-react";
+import { AUTH_URL } from "../api/Login";
 import React from "react";
 
 const SpotifyLogin = () => {
-  return <div>SpotifyLogin</div>;
+  return (
+    <div className="rounded-lg shadow-lg p-8 bg-neutral-800 text-white">
+      <h2 className="text-center font-bold text-4xl mb-10">Bem Vindo</h2>
+      <a
+        className="py-3 px-5 bg-neutral-900 rounded text-white flex items-center shadow"
+        href={AUTH_URL}
+      >
+        <SpotifyLogo size={42} className="text-primary-regular" />
+        <p className="text-xl ml-2 font-semibold">Logar com Spotify!</p>
+      </a>
+    </div>
+  );
 };
 
 export default SpotifyLogin;
