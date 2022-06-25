10207320f5c7c7351d47d9345f2ca0b888d2a73a
Guilherme <guilherme.dsveiga@gmail.com>
2022-06-25 04:00:56 -0300

    adding header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,33 @@
+import { MagnifyingGlass, SpotifyLogo } from "phosphor-react";
+import React, { useState } from "react";
+
+export const Header = () => {
+  const [search, setSearch] = useState("");
+
+  const handleSearch = () => {
+    console.log(search);
+  };
+
+  return (
+    <header className="flex justify-between items-center bg-neutral-dark text-white py-1 md:px-10 px-5 shadow-lg">
+      <SpotifyLogo size={42} className="text-primary-regular" />
+      <div className="relative md:w-1/4 w-2/4">
+        <input
+          type={"text"}
+          className=" py-1 pl-2 pr-[34px] text-white border-b-2 border-primary-regular bg-transparent w-full text-xl"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          onKeyDown={(k) =>
+            (k.code === "Enter" || k.code === "NumpadEnter") &
+              (search.length > 0) && handleSearch()
+          }
+        />
+        <MagnifyingGlass
+          size={24}
+          className="absolute right-2 top-1 cursor-pointer text-primary-regular font-bold"
+          onClick={handleSearch}
+        />
+      </div>
+    </header>
+  );
+};
